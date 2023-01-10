9aad8735a71cdd4a9c3a257d2fa754f754f652aa
Job Alvarez <jobitoalv@gmail.com>
2023-01-10 10:34:25 +1000

    bug fix closing the return

--- src/App.js
+++ src/App.js
@@ -6,25 +6,30 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
-  return <section className='section'>
-    <div className='title'>
-      <h2>
-        <span>/</span>reviews
-      </h2>
-    </div>
-    <div className='section-center'>
-      {people.map((person,personIndex) => {
-        const{id, image, name, title, quote } = person;
+  return (
+    <section className='section'>
+      <div className='title'>
+       <h2>
+         <span>/</span>reviews
+       </h2>
+      </div>
+     <div className='section-center'>
+       {people.map((person,personIndex) => {
+         const{id, image, name, title, quote } = person;
 
-        return <article key={id}>
-          <img src={image} alt={name} className='person-img'/>
-          <h4>{name}</h4>
-          <p className='title'>{title}</p>
-          <p className='quote'>{quote}</p>
-        </article>
+        return (
+          <article key={id}>
+           <img src={image} alt={name} className='person-img'/>
+            <h4>{name}</h4>
+            <p className='title'>{title}</p>
+            <p className='quote'>{quote}</p>
+            <FaQuoteRight className='icon'/>
+          </article>
+        )
       })}
     </div>
-  </section>;
+  </section>
+  )
 }
 
 export default App;
